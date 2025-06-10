5c50b7072567c1b6fbe3577b17738aaf9ca3e8ff
BOJAN SEKULOVSKI <bojansekulovski@BOJANs-iMac.local>
2025-06-10 23:50:12 +0200

    disable eslint during build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
-  // your other config stays here
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
